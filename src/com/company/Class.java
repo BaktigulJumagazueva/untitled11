90908ef0b44e9b27e7a1d0fc41b6f9d015a97ada
13 <baktyguljumagazyeva@gmail.com>
2022-08-20 22:37:03 +0600

    Tapchurma 13

--- src/com/company/Class.java
+++ src/com/company/Class.java
@@ -1,2 +1,23 @@
-package com.company;public class Class {
+package com.company;
+
+public class Class {
+    private int number;
+    private String soz;
+    private int massiv[];
+    public Class (int number, String soz, int ...massiv){
+        this.number=number;
+        this.soz=soz;
+        this.massiv=massiv;
+    }
+    public int setNumber(){
+        return number;
+    }
+    public String setSoz(){
+        return soz;
+    }
+    public int[] setMassiv(){
+        return massiv;
+    }
+
+
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,13 @@
+package com.company;
+
+import java.util.Arrays;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args){
+        Class a= new Class(34,"family",34,24,43,54,6);
+        System.out.println(a.setNumber());
+        System.out.println(a.setSoz());
+        System.out.println(Arrays.toString(a.setMassiv()));
     }
+
 }
